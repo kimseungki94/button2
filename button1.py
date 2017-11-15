748d4c8d50e8c830667a44020da847e7e691a4f1
Seungki Kim <skkim0832@naver.com>
2017-11-15 14:21:23 +0900

    birthday

--- button1.py
+++ button1.py
@@ -4,8 +4,10 @@ window =Tk()
 
 b1 = Button(window, text="kimseungki")
 b2 = Button(window, text="60155346")
+b3 = Button(window, text="0201")
 
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
+b3.grid(row=1, column=0)
 
 window.mainloop()
